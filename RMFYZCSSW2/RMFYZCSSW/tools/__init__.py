5ba04324e0ba61beca572f71cf61df901f12effe
Songsong <453045311@qq.com>
2019-09-24 15:12:59 +0800

    first commit

--- RMFYZCSSW2/RMFYZCSSW/tools/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-#__author__: wangke
-#__date__: 2019/7/25  15:35
-#__ide__: PyCharm
--- RMFYZCSSW2/start.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-#__author__: wangke
-#__date__: 2019/7/24  16:23
-#__ide__: PyCharm
-from scrapy import cmdline
-cmdline.execute("scrapy crawl sfpm".split())
